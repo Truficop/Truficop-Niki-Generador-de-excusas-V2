34e3f6579edc45f9df1db0eabb23b68051918bf3
Nikita Malykhin <43642381+Truficop@users.noreply.github.com>
2024-06-07 13:18:03 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,30 +5,40 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function excuseGenerator(whoList, actionList, whatList, whenList) {
+  const who = Math.floor(Math.random() * whoList.length);
+  const action = Math.floor(Math.random() * actionList.length);
+  const what = Math.floor(Math.random() * whatList.length);
+  const when = Math.floor(Math.random() * whenList.length);
+  return (
+    whoList[who] +
+    " " +
+    actionList[action] +
+    " " +
+    whatList[what] +
+    " " +
+    whenList[when]
+  );
+}
+
 window.onload = function() {
   //write your code here
-  let who = ["The dog", "My grandma", "The mailman", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "my phone", "the car"];
-  let when = [
+  const WHO_LIST = ["The dog", "My grandma", "The mailman", "My bird"];
+  const ACTION_LIST = ["ate", "peed", "crushed", "broke"];
+  const WHAT_LIST = ["my homework", "my phone", "the car"];
+  const WHEN_LIST = [
     "before the class",
     "when I was sleeping",
     "while I was exercising",
     "during my lunch",
     "while I was praying"
   ];
-  let randomWho = Math.floor(Math.random() * who.length);
-  let randomAction = Math.floor(Math.random() * action.length);
-  let randomWhat = Math.floor(Math.random() * what.length);
-  let randomWhen = Math.floor(Math.random() * when.length);
-  let randomExcuse =
-    who[randomWho] +
-    " " +
-    action[randomAction] +
-    " " +
-    what[randomWhat] +
-    " " +
-    when[randomWhen];
+  const randomExcuse = excuseGenerator(
+    WHO_LIST,
+    ACTION_LIST,
+    WHAT_LIST,
+    WHEN_LIST
+  );
   console.log(randomExcuse);
   document.getElementById("excuse").innerHTML = randomExcuse;
 };
